c2bc55540bbb2b1215b7a0fb71dc4b4fadb31a09
taylor <tayjaybabee@gmail.com>
2020-06-29 10:07:20 -0500

    Expand code-base
    - Added comments
    
    - *get_members*:
        - Renamed to 'format_members'
    
    - *format_members*:
        - Renamed from 'get_members'
        - Returns a tuple instead of a single result.

--- ada_hub/lib/helpers/debug.py
+++ ada_hub/lib/helpers/debug.py
@@ -5,21 +5,26 @@ A module containing various functions that assist in debugging.
 """
 
 
-def get_members(dir_res, inc_priv=False):
+def format_members(dir_res, inc_priv=False):
     """
 
     A module to get a sorted, formatted list of the requesters members
 
     Args:
-        inc_priv ():
-        dir_res ():
+        inc_priv (bool): Should this function include exposed private members in it's returned tuple?
+        dir_res (list): The list the caller receives after calling 'dir()'
 
     Returns:
-        list: Returns a list of requesters members. Optionally, does not filter private members
+        Tuple: Returns a list of requesters members. Optionally, does not filter private members
 
     """
+
+    # Prepare an empty list to transfer the received member names to after manipulating it appropriately
     member_list = []
 
+    # Iterate through the received member list
+    #   - If we are not including private members in our return we will refrain from adding members that start with
+    #     '__' to the end-result
     for member in dir_res:
         if not inc_priv and member.startswith('__'):
             continue
@@ -28,4 +33,6 @@ def get_members(dir_res, inc_priv=False):
 
     member_list.sort()
 
-    return member_list
+    mem_list_str = ','.join(member_list)
+
+    return member_list, mem_list_str
