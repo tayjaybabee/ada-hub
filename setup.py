a77772147dd8beb7c68bcc2bcd217d567f539195
taylor <tayjaybabee@gmail.com>
2020-07-10 20:33:43 -0500

    - Fixed inability to package, should now be installable through test.pypi.org
    - setup.py grabs version from pyproject.toml
    
    - added __init__.py to PROG_ROOT/ada_hub/lib/gui/assets and PROG_ROOT/ada_hub/lib/gui/assets/icon_sets
    
    - Changed the name of the executable to ada-hub instead of ada-hub.py
    
    - Added CHANGELOG.md
    
    - First official alpha

--- setup.py
+++ setup.py
@@ -1,19 +1,30 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
+import toml
+import os
 
+toml_file = 'pyproject.toml'
+
+if os.path.exists(toml_file) and os.path.isfile(toml_file):
+    setup_data = toml.load(toml_file)
+    version = setup_data['tool']['poetry']['version']
+else:
+    version = None
 
 setup(
-        name='ada-hub',
-        version='0.2.0',
-        packages=[ 'ada_hub' ],
-        url='https://softworks.inspyre.tech/ada-hub',
-        license='MIT',
-        author='Taylor-Jayde Blackstone',
-        author_email='t.blackstone@inspyre.tech',
-        description='An application that allows one to monitor their environment.',
-        entry_points={
-                'console_scripts': [
-                        'ada_hub=ada_hub:main',
-                ],
-        },
-        keywords='temperature humidity'
+    install_requires=[
+        "sense-hat==2.2.0",
+        "inspy-logger==1.0.3",
+        "sense-emu==1.1",
+        "pysimpleguiqt==0.35.0",
+    ],
+    name="ada-hub",
+    version=version,
+    packages=find_namespace_packages(),
+    url="https://softworks.inspyre.tech/ada-hub",
+    license="MIT",
+    author="Taylor-Jayde Blackstone",
+    author_email="t.blackstone@inspyre.tech",
+    description="An application that allows one to monitor their environment.",
+    scripts=["scripts/ada-hub"],
+    keywords="temperature humidity",
 )
